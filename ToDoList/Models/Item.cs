086a0f5ae5f1a9631839079dcd8d9d74e4267b28
qh <qianhu@pindous.com>
2018-02-20 14:35:06 -0800

    add database connection in model item and revise the methods

--- ToDoList/Models/Item.cs
+++ ToDoList/Models/Item.cs
@@ -1,4 +1,6 @@
 using System.Collections.Generic;
+using MySql.Data.MySqlClient;
+using ToDoList;
 using System;
 
 namespace ToDoList.Models
@@ -7,13 +9,11 @@ namespace ToDoList.Models
   {
     private string _description;
     private int _id;
-    private static List<Item> _instances = new List<Item> {};
 
-    public Item(string description)
+    public Item(string Description, int Id = 0)
     {
-      _description = description;
-      _instances.Add(this);
-      _id = _instances.Count;
+      _id = Id;
+      _description = Description;
     }
 
     public string GetDescription()
@@ -29,9 +29,28 @@ namespace ToDoList.Models
     {
       return _id;
     }
+
     public static List<Item> GetAll()
     {
-      return _instances;
+        List<Item> allItems = new List<Item> {};
+        MySqlConnection conn = DB.Connection();//A MySqlConnection object basically represents the database using the connection information that we set it to. As discussed at the end of the previous lesson, calling DB.Connection() establishes a connection with our MySql database, returning a conn object we can use to interact with the database.
+        conn.Open();//attempt to interact with a database connection that hasn't been manually opened, an exception will be thrown.
+        MySqlCommand cmd = conn.CreateCommand() as MySqlCommand;//for sending SQL statements to the database.
+        cmd.CommandText = @"SELECT * FROM items;";
+        MySqlDataReader rdr = cmd.ExecuteReader() as MySqlDataReader;// the rdr object represents the actual reading of the SQL database. And the MySqlDataReader class also includes a built-in Read() method that sends the SQL Commands to the database and collects whatever the database returns in response to those commands.
+        while(rdr.Read())
+        {
+          int itemId = rdr.GetInt32(0);
+          string itemDescription = rdr.GetString(1);
+          Item newItem = new Item(itemDescription, itemId);
+          allItems.Add(newItem);
+        }//two methods on the rdr object; GetInt32() and GetString()
+        conn.Close();//to close the connection to the database
+        if (conn != null)
+        {
+            conn.Dispose();
+        }
+        return allItems;
     }
     public static void ClearAll()
     {
